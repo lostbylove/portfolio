9fe11a2c5dbba12eacb860830a415cf24522ab4a
Roon Le <roonphile90@gmail.com>
2025-02-03 21:54:06 -0800

    Portfolio Page Change, click on image enlarge functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,11 @@
 class PagesController < ApplicationController
   def welcome
-
   end
-
   def about
-
   end
   def portfolio
-
   end
-
   def contact
     @contact_form = ContactForm.new
   end
-
 end
